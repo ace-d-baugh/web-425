53ec2a87d8e2f2189a22795d5f75c5f1176c6efd
Ace Baugh <ace.d.baugh@gmail.com>
2023-01-03 16:37:06 -0500

    Adding comments

--- profile-app/src/app/app.component.ts
+++ profile-app/src/app/app.component.ts
@@ -1,10 +1,24 @@
+/*
+============================================
+, Title: app.component.ts
+; Author: Ace Baugh
+; Date: 03 January 2023
+; Description: This component is the root component for the hello-world app.
++===========================================
+*/
+
+// import component from angular core
 import { Component } from '@angular/core';
 
+// declare the component
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
+
+// export the component class
 export class AppComponent {
+  // variables to pass to the whole app
   assignment: string = `Assignment 1.5 - Components`;
 }
