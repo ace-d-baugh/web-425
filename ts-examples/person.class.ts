20cabf66489c3b1c8f1456713ec71685f6f8f9f2
Ace Baugh <ace.d.baugh@gmail.com>
2023-01-03 12:35:00 -0500

    Adding more comments

--- ts-examples/person.class.ts
+++ ts-examples/person.class.ts
@@ -1,14 +1,18 @@
 import { IPerson } from './person.interface';
 
+// Person class implements IPerson interface
 class Person implements IPerson {
 	firstName: string;
 	lastName: string;
 
+	// First name and last name are required parameters for the constructor
 	constructor(firstName: string, lastName: string) {
 		this.firstName = firstName;
 		this.lastName = lastName;
 	}
 }
 
+// Create a new Person object
 let myName = new Person('Ace', 'Baugh');
+// Display the person's name
 console.log(`My name is ${myName.firstName} ${myName.lastName}`);
--- ts-examples/person.interface.ts
+++ ts-examples/person.interface.ts
@@ -1,3 +1,4 @@
+// Purpose: To demonstrate how to create an interface in TypeScript
 export interface IPerson {
    firstName: string;
    lastName: string;
