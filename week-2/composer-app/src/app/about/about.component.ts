646f67ce3a65cfd7c98b32f8e96c7887ddf52f0d
Ace Baugh <ace.d.baugh@gmail.com>
2023-01-09 16:34:42 -0500

    added icons and contact info

--- week-2/composer-app/src/app/about/about.component.ts
+++ week-2/composer-app/src/app/about/about.component.ts
@@ -1,10 +1,17 @@
+/*
+============================================
+, Title: about.component.ts
+; Author: Ace Baugh
+; Date: 09 January 2023
+; Description: About component
++===========================================
+*/
+
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-about',
   templateUrl: './about.component.html',
-  styleUrls: ['./about.component.css']
+  styleUrls: ['./about.component.css'],
 })
-export class AboutComponent {
-
-}
+export class AboutComponent {}
