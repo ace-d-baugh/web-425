536ace190e306bc16b09a8acca4902ff97d399af
Ace Baugh <ace.d.baugh@gmail.com>
2023-01-18 12:56:53 -0500

    Added interface and class

--- week-3/enhanced-composer-app/src/app/composer-list/composer-list.component.ts
+++ week-3/enhanced-composer-app/src/app/composer-list/composer-list.component.ts
@@ -9,26 +9,8 @@
 
 // import component and OnInit from angular core
 import { Component, OnInit } from '@angular/core';
-
-// create a class for composers
-export default class Composer {
-  fullName: string;
-  genre: string;
-
-
-  // constructor
-  constructor(fullName: string, genre: string) {
-    this.fullName = fullName;
-    this.genre = genre;
-  }
-
-  // method to output the person's details
-  toString() {
-    console.log(
-      `Full Name: ${this.fullName}\nGenre: ${this.genre}`
-    );
-  }
-}
+import { IComposer } from '../composer.interface';
+import { Composer } from '../composer.class';
 
 @Component({
   selector: 'app-composer-list',
@@ -36,17 +18,11 @@ export default class Composer {
   styleUrls: ['./composer-list.component.css'],
 })
 export class ComposerListComponent implements OnInit {
-  composers: Array<Composer>;
+
+  composers: Array<IComposer>;
 
   constructor() {
-    // create array of composers
-    this.composers = [
-      new Composer('Adam Young', 'Synth Pop'),
-      new Composer('Ashleigh Ball', 'Indie Rock'),
-      new Composer('Brady Parks', 'Indie Folk'),
-      new Composer('David Gahan', 'Synth Pop'),
-      new Composer('Isao Tomita', 'Electronic'),
-    ];
+    this.composers = new Composer().getComposers();
   }
 
   ngOnInit(): void {}
