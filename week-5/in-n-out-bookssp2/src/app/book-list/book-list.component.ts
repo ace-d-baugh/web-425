64a97a347009c7d5189d6f061d4254d24fd4e132
Ace Baugh <ace.d.baugh@gmail.com>
2023-01-31 18:30:32 -0500

    Setup for 5.3

---
+++ week-5/in-n-out-bookssp2/src/app/book-list/book-list.component.ts
@@ -0,0 +1,17 @@
+/*
+============================================
+, Title: book-list.component.ts
+; Author: Ace Baugh
+; Date: 31 January 2023
+; Description: This is the book list component
++===========================================
+*/
+
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-book-list',
+  templateUrl: './book-list.component.html',
+  styleUrls: ['./book-list.component.scss'],
+})
+export class BookListComponent {}
