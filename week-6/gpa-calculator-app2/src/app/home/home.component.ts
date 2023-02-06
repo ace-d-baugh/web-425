0fa1187407ee0b5bb8c7a656c472f7c47447d451
Ace Baugh <ace.d.baugh@gmail.com>
2023-02-06 14:26:07 -0500

    Added grades

--- week-6/gpa-calculator-app2/src/app/home/home.component.ts
+++ week-6/gpa-calculator-app2/src/app/home/home.component.ts
@@ -8,10 +8,39 @@
 */
 
 import { Component } from '@angular/core';
+import { ITranscript } from '../transcript.interface';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
 })
-export class HomeComponent {}
+export class HomeComponent {
+  transcriptEntry: ITranscript;
+
+  selectableGrades: Array<string> = [
+    'A',
+    'A-',
+    'B+',
+    'B',
+    'B-',
+    'C+',
+    'C',
+    'C-',
+    'D+',
+    'D',
+    'D-',
+    'F',
+  ];
+
+  transcriptEntries: Array<ITranscript> = [];
+
+  constructor() {
+    this.transcriptEntry = {} as ITranscript;
+  }
+
+  saveEntry() {
+    this.transcriptEntries.push(this.transcriptEntry);
+    this.transcriptEntry = {} as ITranscript;
+  }
+}
---
+++ week-6/gpa-calculator-app2/src/app/transcript.interface.ts
@@ -0,0 +1,13 @@
+/*
+============================================
+; Title: transcript.interface.ts
+; Author: Ace Baugh
+; Date: February 6, 2023
+; Description: This is the transcript interface
+============================================
+*/
+
+export interface ITranscript {
+  course: string;
+  grade: string;
+}
