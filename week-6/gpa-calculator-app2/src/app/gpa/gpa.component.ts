f596761b2df3ae947167813de9294f8baeb58643
Ace Baugh <ace.d.baugh@gmail.com>
2023-02-06 14:16:56 -0500

    Fixing dates and comments

--- week-6/gpa-calculator-app2/src/app/gpa/gpa.component.ts
+++ week-6/gpa-calculator-app2/src/app/gpa/gpa.component.ts
@@ -1,10 +1,17 @@
+/*
+============================================
+; Title: gpa.component.ts
+; Author: Ace Baugh
+; Date: February 6, 2023
+; Description: This is the gpa component file
+============================================
+*/
+
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-gpa',
   templateUrl: './gpa.component.html',
-  styleUrls: ['./gpa.component.scss']
+  styleUrls: ['./gpa.component.scss'],
 })
-export class GpaComponent {
-
-}
+export class GpaComponent {}
--- week-6/gpa-calculator-app2/src/app/grade-summary/grade-summary.component.ts
+++ week-6/gpa-calculator-app2/src/app/grade-summary/grade-summary.component.ts
@@ -1,10 +1,17 @@
+/*
+============================================
+; Title: grade-summary.component.ts
+; Author: Ace Baugh
+; Date: February 6, 2023
+; Description: This is the grade summary component file
+============================================
+*/
+
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-grade-summary',
   templateUrl: './grade-summary.component.html',
-  styleUrls: ['./grade-summary.component.scss']
+  styleUrls: ['./grade-summary.component.scss'],
 })
-export class GradeSummaryComponent {
-
-}
+export class GradeSummaryComponent {}
