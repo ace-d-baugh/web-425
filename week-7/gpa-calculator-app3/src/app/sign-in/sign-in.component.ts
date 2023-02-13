0b483896e0e9584dff88306f3d8bf285802d6df2
Ace Baugh <ace.d.baugh@gmail.com>
2023-02-13 15:44:32 -0500

    Sign In form created

--- week-7/gpa-calculator-app3/src/app/sign-in/sign-in.component.ts
+++ week-7/gpa-calculator-app3/src/app/sign-in/sign-in.component.ts
@@ -7,11 +7,46 @@
 ============================================
 */
 
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { Router } from '@angular/router';
+import { CookieService } from 'ngx-cookie-service';
+import { SignInService } from '../sign-in.service';
 
 @Component({
   selector: 'app-sign-in',
   templateUrl: './sign-in.component.html',
   styleUrls: ['./sign-in.component.scss'],
 })
-export class SignInComponent {}
+export class SignInComponent {
+
+  // Variables
+  signInForm: FormGroup = new FormGroup({});
+  errorMessage: string = '';
+
+  constructor(private router: Router, private cookieService: CookieService, private fb: FormBuilder, private signinService: SignInService) { }
+
+  // This function is called when the component is initialized
+  ngOnInit() {
+    this.signInForm = this.fb.group({
+      studentId: [null]
+    });
+  }
+
+  // This function is called when the user clicks the submit button
+  onSubmit() {
+    // Get the form values
+    const formValues = this.signInForm.value;
+    const studentId = parseInt(formValues.studentId);
+
+    // If the student ID is valid, set the cookie and redirect to the home page
+    if (this.signinService.validate(studentId)) {
+      this.cookieService.set('session_user', studentId.toString(), 1);
+      this.router.navigate(['/']);
+    }
+    // Otherwise, display an error message
+    else {
+      this.errorMessage = 'The student ID you entered is invalid, please try again.';
+    }
+  }
+}
